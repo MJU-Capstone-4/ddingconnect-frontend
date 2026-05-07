1f0700376a39fd447517a191b7a44d4ea6983e5d
hello-yujin <yuuuujiiiin@naver.com>
2026-05-07 10:52:39 +0900

    init: SVG 아이콘 디렉토리 구성 및 예시 아이콘 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,10 @@
+import CheckIcon from '@/shared/assets/icons/check.svg?react';
+
 export function App() {
   return (
     <div>
       <h1>DDingConnect</h1>
+      <CheckIcon width={48} height={48} />
     </div>
   );
 }
