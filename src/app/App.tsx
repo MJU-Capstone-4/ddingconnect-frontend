2cbc00342bf5aea361337910137c4c160d52fb90
hello-yujin <yuuuujiiiin@naver.com>
2026-05-07 11:17:51 +0900

    chore: App 컴포넌트에서 SVGR 확인용 임시 코드 제거

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,7 @@
-import CheckIcon from '@/shared/assets/icons/check.svg?react';
-
 export function App() {
   return (
     <div>
       <h1>DDingConnect</h1>
-      <CheckIcon width={48} height={48} />
     </div>
   );
 }
