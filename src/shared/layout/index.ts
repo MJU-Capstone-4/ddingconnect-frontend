aaeac6a98db69fd08bffe75ff4c3867d656da8a9
hello-yujin <yuuuujiiiin@naver.com>
2026-05-13 18:10:25 +0900

    feat: Header 컴포넌트 barrel export 추가

--- src/shared/layout/index.ts
+++ src/shared/layout/index.ts
@@ -1,3 +1,7 @@
 export { AppHeader } from './app-header';
 export { BottomNav } from './bottom-nav';
+export { MainHeader } from './header';
+export type { MainHeaderProps } from './header';
+export { SubHeader } from './header';
+export type { SubHeaderProps } from './header';
 export { PageLayout } from './page-layout';
