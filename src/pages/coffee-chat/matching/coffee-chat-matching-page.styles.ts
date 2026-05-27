a5c3aec34a6137cb55f60caee6c28da0053f9db5
hello-yujin <yuuuujiiiin@naver.com>
2026-05-27 19:08:57 +0900

    refactor: formCard 배경색을 bg-white에서 시맨틱 토큰 bg-surface로 교체

--- src/pages/coffee-chat/matching/coffee-chat-matching-page.styles.ts
+++ src/pages/coffee-chat/matching/coffee-chat-matching-page.styles.ts
@@ -2,4 +2,4 @@ export const page = 'flex flex-col pb-8';
 
 export const heroBreakout = '-mx-[18px] -mt-[24px] w-[calc(100%+36px)]';
 
-export const formCard = 'mt-5 bg-white rounded-2xl';
+export const formCard = 'mt-5 bg-surface rounded-2xl';
